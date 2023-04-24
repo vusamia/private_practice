b145eaaf878fa3aa75d955ea1bf2ae9de51ab8f0
Snorine <vusamia.snorine@gmail.com>
2023-04-24 14:46:27 -0700

    Ptrint single digit numbers of base 10 using putchar

---
+++ 0x01-variables_if_else_while/6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints single digit numbers of base 10
+ *
+ *Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + '0');
+		i++;
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
